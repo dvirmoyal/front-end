4ee43be47715630d32aca91c890b8745ed8af266
dvirmoyal <dvirmoyal25@gmail.com>
2025-03-05 20:58:04 +0200

    deploy to s3

--- calculator-frontend/src/constants/constants.js
+++ calculator-frontend/src/constants/constants.js
@@ -21,8 +21,8 @@ export const calculatorTypes = {
 // קבועים לתקשורת עם המיקרוסרויסים
 export const API_CONSTANTS = {
   // כתובות השירותים (יש להחליף בהתאם לסביבת העבודה)
-  ADDITION_SERVICE_URL: process.env.REACT_APP_ADDITION_SERVICE_URL || 'http://localhost:8000',
-  MULTIPLICATION_SERVICE_URL: process.env.REACT_APP_MULTIPLICATION_SERVICE_URL || 'http://localhost:8080',
+  ADDITION_SERVICE_URL: process.env.REACT_APP_ADDITION_SERVICE_URL
+  MULTIPLICATION_SERVICE_URL: process.env.REACT_APP_MULTIPLICATION_SERVICE_URL || 'http://localhost:8090',
 
   // זמני Timeout לבקשות
   REQUEST_TIMEOUT_MS: 5000,
